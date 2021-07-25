ba7cdd3c59ea6ac7b6d4531685cdd45eb1753ad7
xobotun <me@xobotun.com>
2021-07-25 23:53:36 +0300

    Added fields for a generic panel from sources. But I'm not sure if they are always used.

--- src/main/java/com/xobotun/grafanagen/model/grafana/panel/AbstractPanel.kt
+++ src/main/java/com/xobotun/grafanagen/model/grafana/panel/AbstractPanel.kt
@@ -2,15 +2,42 @@ package com.xobotun.grafanagen.model.grafana.panel
 
 /**
  * An abstract widget with bare minumum of fields.
- * TODO: find out what is the bare minimum of fields. Official documentation is surprisingly unhelpful.
+ *
+ * [Source](https://github.com/grafana/grafana/blob/v6.2.x/public/app/features/dashboard/state/PanelModel.ts#L78-L110)
+ *
  */
 abstract class AbstractPanel(
-    /** Widget's title. */
-    val title: String,
+    /** Widget's id on the dashboard? TODO */
+    val id: Int,
     /** Widget's bounding box. */
     val gridPos: PanelDimensions,
-    /** Widget's id on the dashboard? TODO */
-    val id: Int
+    /** Widget's title. */
+    val title: String,
+    val alert: Any? = null,
+    val scopedVars: Any? = null,
+    val repeat: String? = null,
+    val repeatIteration: Int? = null,
+    val repeatPanelId: Int? = null,
+    val repeatDirection: String? = null,
+    val repeatedByRow: Boolean? = null,
+    val maxPerRow: Int? = null,
+    val collapsed: Boolean? = null,
+    val panels: Any? = null,
+    val soloMode: Boolean? = null,
+    val targets: Any? = null,
+    val datasource: String? = null,
+    val thresholds: Any? = null,
+    val pluginVersion: String? = null,
+    val snapshotData: Any? = null,
+    val timeFrom: Any? = null,
+    val timeShift: Any? = null,
+    val hideTimeOverride: Any? = null,
+    val options: Any? = null,
+    val maxDataPoints: Int? = null,
+    val interval: String? = null,
+    val description: String? = null,
+    val links: List<Any>? = null,
+    val transparent: Boolean = false,
 ) {
     /** Widget's type that determines its' fields and display mode. TODO: get a list of non-plugins types somewhere. */
     abstract val type: String
--- src/main/java/com/xobotun/grafanagen/model/grafana/panel/TextPanel.kt
+++ src/main/java/com/xobotun/grafanagen/model/grafana/panel/TextPanel.kt
@@ -1,20 +1,31 @@
 package com.xobotun.grafanagen.model.grafana.panel
 
 /**
- * A simple text panel.
+ * A simple text panel. Comes from [here](https://github.com/grafana/grafana/blob/v6.2.x/public/app/plugins/panel/text/module.ts#L24-L25).
+ * Or [here](https://github.com/grafana/grafana/blob/v6.2.x/public/app/plugins/panel/text2/types.ts). Why are there two copies anyway?
  */
 class TextPanel(
-    title: String,
-    gridPos: PanelDimensions,
     id: Int,
-    /** Text display mode. TODO: find more modes */
-    val mode: String = "markdown",
+    gridPos: PanelDimensions,
+    title: String,
+    /** Text display mode. */
+    val mode: Mode = Mode.MARKDOWN,
     /** Text content. TODO: probably needs support for variables and queries. */
     val content: String
 ) : AbstractPanel(
-    title,
+    id,
     gridPos,
-    id
+    title
 ) {
     override val type = "text"
+
+    /** Internal enum for [TextPanel]. Dictates the way content is shown. Pretty obvious. */
+    enum class Mode(
+        /** Just for the sake of same style across the project. */
+        val shownName: String
+    ) {
+        HTML("html"),
+        MARKDOWN("markdown"),
+        TEXT("text"),
+    }
 }
