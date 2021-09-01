044f5dd878b404a3465dccf8603bae396b76534f
xobotun <me@xobotun.com>
2021-09-01 23:01:28 +0300

    Treating non-existent and null fields as the same. Also treating `transparent` set to `false` or `null` as the same value.

--- src/test/java/com/xobotun/grafanagen/serde/SerdeTest.kt
+++ src/test/java/com/xobotun/grafanagen/serde/SerdeTest.kt
@@ -4,7 +4,6 @@ import com.google.common.collect.Maps
 import com.xobotun.grafanagen.serde.panel.textPanel1
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
-import kotlin.test.assertEquals
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
@@ -26,29 +25,44 @@ class SerdeTest {
             val parsed = JsonParser.parseString(it.raw)
             val decomposed = gson.toJsonTree(it.value, it.type.type)
 
-            try {
-                assertEquals(decomposed, parsed)
-            } catch (e: AssertionError) {
-                if (!(parsed.isJsonObject && decomposed.isJsonObject)) {
-                    throw AssertionError("This test is supposed to compare only objects, but parsed was ${parsed::class.java} and decomposed was ${decomposed::class.java}", e)
-                }
-
-                val parsedMap = parsed.asJsonObject.asMap()
-                val decomposedMap = decomposed.asJsonObject.asMap()
-
-                val difference = Maps.difference(decomposedMap, parsedMap)
-                throw AssertionError(difference.toString(), e)
-            }
+            assertJsonsEquals(decomposed, parsed)
         }
     }
 
     /**
+     * Runs a custom map comparison of two json objects.
+     */
+    private fun assertJsonsEquals(expected: JsonElement, actual: JsonElement) {
+        if (!(actual.isJsonObject && expected.isJsonObject)) {
+            throw AssertionError("This test is supposed to compare only objects, but actual was ${actual::class.java} and expected was ${expected::class.java}")
+        }
+
+        val actualMap = actual.asJsonObject.asMap()
+        val expectedMap = expected.asJsonObject.asMap()
+
+        val difference = Maps.difference(expectedMap, actualMap)
+        throw AssertionError(difference.toString())
+    }
+
+    /**
      * Accesses an internal map so there's no need to call [JsonObject.entrySet] to construct another map from it.
+     * Omits null values.
+     * Treats `transparent` set to `false` as `null`. TODO: move it to some kind of equivalency classes out of this method. It does not belong here.
      */
     private fun JsonObject.asMap() = this.let {
         val innerTree = JsonObject::class.java.getDeclaredField("members")
         innerTree.isAccessible = true
 
         return@let innerTree.get(this) as LinkedTreeMap<String, JsonElement>
+    }
+        .filterValues { !it.isJsonNull }
+        .toMutableMap()
+        .also {
+            it.removeKeyIfValueMatches("transparent") { (it?.isJsonPrimitive ?: false) && !it!!.asBoolean }
+        }
+
+    private fun <K, V> MutableMap<K, V>.removeKeyIfValueMatches(key: K?, valueMatcher: (V?) -> Boolean) {
+        val value = this[key]
+        if (valueMatcher(value)) this.remove(key)
     }
 }
